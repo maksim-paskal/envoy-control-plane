b16ca36c78e54c3359981a00e81d86cd707e6c53
devops.maksim.p <devops.maksim.p@alldigitalads.com>
2021-03-17 13:58:26 +0200

    add drainEnvoy cli command

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -17,6 +17,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
@@ -25,13 +26,16 @@ import (
 )
 
 var (
-	action    = flag.String("action", "/api/zone", "api action")
-	namespace = flag.String("namespace", os.Getenv("MY_POD_NAMESPACE"), "pod namespace")
-	pod       = flag.String("pod", os.Getenv("HOSTNAME"), "pod name")
-	server    = flag.String("server", "envoy-control-plane", "controlplane host")
-	port      = flag.Int("port", 18081, "controlplane port")
-	wait      = flag.Bool("wait", true, "wait controlplane")
-	debug     = flag.Bool("debug", false, "debug mode")
+	action         = flag.String("action", "/api/zone", "api action")
+	namespace      = flag.String("namespace", os.Getenv("MY_POD_NAMESPACE"), "pod namespace")
+	pod            = flag.String("pod", os.Getenv("HOSTNAME"), "pod name")
+	server         = flag.String("server", "envoy-control-plane", "controlplane host")
+	port           = flag.Int("port", 18081, "controlplane port")
+	wait           = flag.Bool("wait", true, "wait controlplane")
+	debug          = flag.Bool("debug", false, "debug mode")
+	drainEnvoy     = flag.Bool("drainEnvoy", false, "drain envoy")
+	envoyAdminPort = flag.Int("envoyAdminPort", 18000, "envoy admin port")
+	logFlags       = flag.Int("logFlags", 0, "log flags")
 )
 
 func waitForAPI() {
@@ -67,15 +71,49 @@ func waitForAPI() {
 	}
 }
 
+func requestEnvoyAdmin(method string, path string) {
+	cli := &http.Client{}
+	ctx := context.Background()
+	url := fmt.Sprintf("http://127.0.0.1:%d%s", *envoyAdminPort, path)
+
+	req, err := http.NewRequestWithContext(ctx, method, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := cli.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp.Body.Close()
+
+	log.Printf("[%s] %s", path, string(body))
+}
+
 func main() {
 	flag.Parse()
 
+	log.SetFlags(*logFlags)
+
+	if *drainEnvoy {
+		requestEnvoyAdmin(http.MethodPost, "/drain_listeners?graceful")
+		requestEnvoyAdmin(http.MethodPost, "/healthcheck/fail")
+
+		return
+	}
+
 	if len(*namespace) == 0 {
-		panic("no namespace")
+		log.Fatal("no namespace")
 	}
 
 	if len(*pod) == 0 {
-		panic("no pod")
+		log.Fatal("no pod")
 	}
 
 	if *wait {
@@ -93,28 +131,28 @@ func main() {
 
 	req, err := http.NewRequestWithContext(ctx, http.MethodPost, requestURL, strings.NewReader(data.Encode()))
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
 	resp, err := cli.Do(req)
 	if err != nil {
-		panic(err)
-	}
-
-	if resp.Body != nil {
-		defer resp.Body.Close()
+		log.Fatal(err)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+	}
+
+	if resp.Body != nil {
+		resp.Body.Close()
 	}
 
 	os.Stdout.WriteString(string(body))
 
 	if resp.StatusCode != http.StatusOK {
-		panic("result not ok")
+		log.Fatal("result not ok")
 	}
 }
